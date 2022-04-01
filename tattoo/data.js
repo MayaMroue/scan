13d25a8893625d472fce3619c872feb86c525f74
MayaMroue <87223176+MayaMroue@users.noreply.github.com>
2022-04-01 11:48:39 +0300

    Add files via upload
    description

---
+++ tattoo/data.js
@@ -0,0 +1,42 @@
+
+
+
+// let qs=[
+
+//     {
+//         id:1,
+//         question:"hey",
+//     },
+//     {
+//         id:2,
+//         question:"hey",
+//     }
+// ];
+// localStorage.setItem("QS", JSON.stringify(qs));
+
+// let view=[
+//     {
+
+//     id: "",
+//     name: "",
+//     phone: "",
+//     desc:" ",
+//     img: ""
+
+// }
+
+// ];
+
+
+// let logDB=[
+//     {
+//      id: 1,
+//     username: "maya",
+//     password:"999"
+//         }
+
+ 
+
+// ];
+// localStorage.setItem("log", JSON.stringify(logDB));
+
